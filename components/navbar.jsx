1f6948592bb2c1c9da6b2b6f8d18d4343cd29c37
theneonstark <mohd47149@gmail.com>
2025-06-30 16:41:29 +0530

    changes in navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar() {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <Link href="/" className="text-2xl font-bold text-primary">
-            ModernServices
+            InfiniMorph Consulting
           </Link>
 
           <div className="hidden md:flex space-x-1">
