f5f5300bb28cbffd5c9af60bd542cecb5c71f98d
theneonstark <mohd47149@gmail.com>
2025-08-01 12:06:14 +0530

    SEO implement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,11 @@ export default function RootLayout({
       <head>
         <meta name="facebook-domain-verification" content="ckrm6ng9qs91ukp9qxhgo7yy08tdxb" />
         <meta name="google-site-verification" content="PUA7PODP-QIn6ZTVvPWEXyBQ6mYjxoHBrNf5BmnZOiU" />
+        <meta name="keywords" content="web development company India, ecommerce development, SEO services, UI UX design agency, mobile app development, startup branding, Infinimorph Consulting" />
         <meta property="og:title" content="InfiniMorph Consulting | All-in-One Digital, Branding & Tech Solutions"/>
         <meta property="og:site_name" content="InfiniMorphConsulting" />
         <meta property="og:url" content="https://www.infinimorphconsulting.com/" />
-        <meta property="og:description" content="Empowering startups and businesses with web development, mobile apps, branding, design, marketing, and business consulting — all under one roof." />
+        <meta name="description" content="Custom web development, mobile apps, UI/UX, SEO, branding & startup solutions. Infinimorph offers all-in-one digital services in India." />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://scontent.fdel3-1.fna.fbcdn.net/v/t39.30808-6/492063890_2097305934029009_3821675367376804443_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=oyGVoeQeg5YQ7kNvwFo0-ES&_nc_oc=AdnCmo7_-i93WEasuF45RxiD-psIJFshRrn7dDp_K-ZQIQNW5q14b_G8rbxr6f34XOOv5oBtohJqBfLJeq3LUeAl&_nc_zt=23&_nc_ht=scontent.fdel3-1.fna&_nc_gid=KigyIyPXUlXgfgFYBJE2Jw&oh=00_AfQfO3dl5X0Kj6LC-L5tb2LniAzSgImVD9_kZpa5_DxIYQ&oe=688FAC77" />
         {/* ✅ Google Analytics Script */}
