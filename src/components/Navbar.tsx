13aea6c856f6caccd65291e9ef42cbc4fbb682b6
theneonstark <mohd47149@gmail.com>
2025-08-29 16:54:27 +0530

    add blog

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,6 +40,7 @@ export const Navbar = () => {
               <Link href="/" className="text-opacity-60 text-white hover:text-opacity-100 transition">Home</Link>
               <Link href="/about" className="text-opacity-60 text-white hover:text-opacity-100 transition">About</Link>
               <Link href="/features" className="text-opacity-60 text-white hover:text-opacity-100 transition">Features</Link>
+              <Link href="/updates" className="text-opacity-60 text-white hover:text-opacity-100 transition">Blog</Link>
               <Link href="/help" className="text-opacity-60 text-white hover:text-opacity-100 transition">Help</Link>
               {/* <Link href="/customers" className="text-opacity-60 text-white hover:text-opacity-100 transition">Customers</Link> */}
               <Link href="/contact">
